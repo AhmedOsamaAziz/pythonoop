86b0d131e83e3bb14469aed799e7df3c0103617a
ahmedosamaaziz <12422020688217@pg.cu.edu.eg>
2022-04-06 19:14:35 +0200

    Getter & Setter

--- item.py
+++ item.py
@@ -1,23 +1,37 @@
 import csv
 
+
 class Item:
     # Clas Attribute >> can be changed
     pay_rate = 0.8
     all = []
 
-    def __init__(self, name: str, price: float, quantity=0.0):
+    def __init__(self, name: str, price: float, quantity=0):
         # Validations
         assert name and not str.isspace(name), f'Name {name} can\'t be null or whitespace'
         assert price >= 0, f'Price {price} should be greater than or equal to 0'
         assert quantity >= 0, f'quantity {quantity} should be greater than or equal to 0'
 
         # Assign Values
-        self.name = name
+        # __ Means >> Private Attribute
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         # Actions
         Item.all.append(self)
+
+    @property
+    # Property Decorator = Read-Only Attribute
+    def name(self):
+        return self.__name
+
+    @name.setter
+    # You cane change the instance name like this >> phone1.name = "Nokia 3310"
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception("The name is too long!")
+        self.__name = value
 
     def calculate_total_price(self):
         return f'{self.name} coast is {self.quantity * self.price}'
@@ -38,13 +52,14 @@ class Item:
             Item(
                 name=item.get('name'),
                 price=float(item.get('price')),
-                quantity=float(item.get('quantity'))
+                quantity=int(item.get('quantity'))
             )
 
     # Does not send a reference to class in the function definition
     # Use static when you don't need an instance to work with
     # This should do something that has a relationship with the class
     # but not something that must be unique per instance
+    # Also could be called from instance itself
     @staticmethod
     def is_integer(num):
         if isinstance(num, float):
@@ -55,8 +70,6 @@ class Item:
             return False
 
     def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
 
-Item.get_all_from_csv()
-print(Item.all)
---
+++ main.py
@@ -0,0 +1,9 @@
+from item import Item
+from phone import Phone
+
+
+phone1 = Phone("Nokia", 500, 5, 1)
+phone1.name = "Nokia 3310"
+# print(phone1.calculate_total_price())
+print(Item.all)
+print(Phone.all)
